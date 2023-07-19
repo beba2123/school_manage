759331349eed0a039efd42ab34735ba02ba00176
mgeza <mgeza@ZOO>
2023-07-19 13:14:11 +0300

    nothing changed

--- school/models.py
+++ school/models.py
@@ -4,4 +4,11 @@ import uuid
 class Student(models.Model):
     id         = models.UUIDField(default=uuid.uuid4, unique= True, primary_key= True, editable= False)
     firstName  = models.CharField(max_length= 50, null = False, blank= False)
+    middelName = models.CharField(max_length= 50, null = False, blank= False)
     lastName   = models.CharField(max_length= 50, null = False, blank= False)
+    email      = models.EmailField(max_length= 100, null = False, blank= False)
+    age        = models.IntegerField(max_length=3, null = False, blank= False)
+    sex        = models.CharField(max_length=1, null = False, blank= False)
+    class_room_id = models.ForeignKey()
+
+class
