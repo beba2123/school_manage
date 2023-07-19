b4b3de56ef917d71b10f739952e2e11728043801
mgeza <mgeza@ZOO>
2023-07-19 12:47:30 +0300

    Student model added

--- school/models.py
+++ school/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+class Student(models.Model):
+    id         = models.UUIDField(default=uuid.uuid4, unique= True, primary_key= True, editable= False)
+    firstName  = models.CharField(max_length= 50, null = False, blank= False)
+    lastName   = models.CharField(max_length= 50, null = False, blank= False)
