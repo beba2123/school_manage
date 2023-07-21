1cfd747b35d99cc4ff712ede93f0eca2cad30ed4
mgeza <mgeza@ZOO>
2023-07-21 19:55:40 +0300

    git fix and teacher table add :)

--- school/models.py
+++ school/models.py
@@ -37,13 +37,20 @@ class Subject(models.Model):
 class Class_room(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     roomName = models.CharField(max_length=50, null=False, blank=False)
-
+class Teacher(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    firstName = models.CharField(max_length=50, null=False, blank=False)
+    middelName = models.CharField(max_length=50, null=False, blank=False)
+    lastName = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=100, null=False, blank=False)
+    phone = models.IntegerField(max_length=12, null=False, blank=False)
+    sex = models.CharField(max_length=1, null=False, blank=False)
 class Class_subject(models.Model):
     classRoomId = models.ForeignKey(Class_room, on_delete=models.CASCADE(), blank=False)
     subjectId = models.ForeignKey(Subject, on_delete=models.CASCADE(), blank=False)
     teacherId = models.ForeignKey(Teacher, on_delete=models.CASCADE(), blank=False)
 class Teacher_subject(models.Model):
-    teacherId = models.ForeignKey(Family_type, on_delete=models.CASCADE(), blank=False)
-    subjectId = models.ForeignKey(Family_type, on_delete=models.CASCADE(), blank=False)
+    teacherId = models.ForeignKey(Teacher, on_delete=models.CASCADE(), blank=False)
+    subjectId = models.ForeignKey(Student, on_delete=models.CASCADE(), blank=False)
 
 
