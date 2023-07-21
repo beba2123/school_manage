ab71978e35242e216ad8cfb9b10c0b4006e948d2
mgeza <mgeza@ZOO>
2023-07-21 20:38:00 +0300

    database classes are registered to admin site

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Teacher, Student, Subject, Family, Family_type, Student_Family, Teacher_subject, Class_room, Class_subject
 # Register your models here.
+
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Subject)
+admin.site.register(Family)
+admin.site.register(Family_type)
+admin.site.register(Student_Family)
+admin.site.register(Teacher_subject)
+admin.site.register(Class_subject)
+
+
+
+
