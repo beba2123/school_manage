bd83320bcbe6c70cae850dfa376a01b756e5542c
mgeza <mgeza@ZOO>
2023-07-22 13:51:14 +0300

    dug fix models py

--- school/admin.py
+++ school/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Teacher, Student, Subject, Family, Family_type, Student_Family, Teacher_subject, Class_room, Class_subject
+from .models import Teacher, Student, Subject, Family, Family_type, Teacher_subject, Class_room, Class_subject
 # Register your models here.
 
 admin.site.register(Teacher)
@@ -7,7 +7,6 @@ admin.site.register(Student)
 admin.site.register(Subject)
 admin.site.register(Family)
 admin.site.register(Family_type)
-admin.site.register(Student_Family)
 admin.site.register(Teacher_subject)
 admin.site.register(Class_subject)
 admin.site.register(Class_room)
--- school/models.py
+++ school/models.py
@@ -4,6 +4,18 @@ import uuid
 class Class_room(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     roomName = models.CharField(max_length=50, null=False, blank=False)
+    def __str__(self):
+        return self.roomName
+class Family(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    firstName = models.CharField(max_length=50, null=False, blank=False)
+    middelName = models.CharField(max_length=50, null=False, blank=False)
+    lastName = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=100, null=False, blank=False)
+    phone = models.IntegerField(null=False, blank=False)
+    sex = models.CharField(max_length=1, null=False, blank=False)
+    def __str__(self):
+        return self.firstName + ' ' ##+ self.Family_type_id.__str__()
 class Student(models.Model):
     id         = models.UUIDField(default=uuid.uuid4, unique= True, primary_key= True, editable= False)
     firstName  = models.CharField(max_length= 50, null = False, blank= False)
@@ -14,28 +26,21 @@ class Student(models.Model):
     age        = models.IntegerField(null = False, blank= False)
     sex        = models.CharField(max_length=1, null = False, blank= False)
     class_room_id = models.ForeignKey(Class_room, on_delete=models.SET_NULL, blank=False, null=True)
-
-
-class Family(models.Model):
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    firstName = models.CharField(max_length=50, null=False, blank=False)
-    middelName = models.CharField(max_length=50, null=False, blank=False)
-    lastName = models.CharField(max_length=50, null=False, blank=False)
-    email = models.EmailField(max_length=100, null=False, blank=False)
-    phone = models.IntegerField(null=False, blank=False)
-    sex = models.CharField(max_length=1, null=False, blank=False)
+    family_id = models.ForeignKey(Family, on_delete=models.SET_NULL, null=True)
+    def __str__(self):
+        return self.firstName + ' ' ##+ self.family_id.__str__()
 
 class Family_type(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     typeName = models.CharField(max_length=50, null=False, blank=False)
-class Student_Family(models.Model):
-    familyTypeId = models.ForeignKey(Family_type, on_delete= models.CASCADE, blank=False)
-    studentId = models.ForeignKey(Student, on_delete= models.CASCADE, blank= False)
-    familyId = models.ForeignKey(Family, on_delete= models.CASCADE, blank= False)
+    def __str__(self):
+        return self.typeName
 
 class Subject(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     subjectName = models.CharField(max_length=50, null=False, blank=False)
+    def __str__(self):
+        return self.subjectName
 
 class Teacher(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
@@ -45,12 +50,18 @@ class Teacher(models.Model):
     email = models.EmailField(max_length=100, null=False, blank=False)
     phone = models.IntegerField( null=False, blank=False)
     sex = models.CharField(max_length=1, null=False, blank=False)
+
+    def __str__(self):
+        return self.firstName
 class Class_subject(models.Model):
-    classRoomId = models.ForeignKey(Class_room, on_delete=models.CASCADE, blank=False)
-    subjectId = models.ForeignKey(Subject, on_delete=models.CASCADE, blank=False)
-    teacherId = models.ForeignKey(Teacher, on_delete=models.CASCADE, blank=False)
+    classRoomId = models.ForeignKey(Class_room, on_delete=models.SET_NULL, null=True, blank=False)
+    subjectId = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True, blank=False)
+    def __str__(self):
+        return self.classRoomId.__str__() + ' ' + self.subjectId.__str__()
 class Teacher_subject(models.Model):
-    teacherId = models.ForeignKey(Teacher, on_delete=models.CASCADE, blank=False)
-    subjectId = models.ForeignKey(Student, on_delete=models.CASCADE, blank=False)
+    teacherId = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True, blank=False)
+    subjectId = models.ForeignKey(Student, on_delete=models.SET_NULL, null=True, blank=False)
+    def __str__(self):
+        return self.teacherId.__str__() + ' ' + self.subjectId.__str__()
 
 
