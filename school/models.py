1ad44f26479f9d9b1eda14d54cd1133ca516ce8a
mgeza <mgeza@ZOO>
2023-07-22 15:00:50 +0300

    new dataBase implemented

--- school/models.py
+++ school/models.py
@@ -6,6 +6,28 @@ class Class_room(models.Model):
     roomName = models.CharField(max_length=50, null=False, blank=False)
     def __str__(self):
         return self.roomName
+class Job_id:
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    title = models.CharField(max_length=50, null=False, blank=False)
+
+class Employees:
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    firstName = models.CharField(max_length=50, null=False, blank=False)
+    middelName = models.CharField(max_length=50, null=False, blank=False)
+    lastName = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=100, null=False, blank=False)
+    phone = models.IntegerField(null=False, blank=False)
+    address = models.CharField(max_length=50, null=False, blank=False)
+    sex = models.CharField(max_length=1, null=False, blank=False)
+    registrationDate = models.DateField(auto_now_add= True)
+    job_id = models.ForeignKey(Job_id)
+
+class Family_type(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    typeName = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return self.typeName
 class Family(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     firstName = models.CharField(max_length=50, null=False, blank=False)
@@ -14,8 +36,9 @@ class Family(models.Model):
     email = models.EmailField(max_length=100, null=False, blank=False)
     phone = models.IntegerField(null=False, blank=False)
     sex = models.CharField(max_length=1, null=False, blank=False)
+    family_type_id = models.ForeignKey(Family_type, on_delete= models.SET_NULL,null=True)
     def __str__(self):
-        return self.firstName + ' ' ##+ self.Family_type_id.__str__()
+        return self.firstName
 class Student(models.Model):
     id         = models.UUIDField(default=uuid.uuid4, unique= True, primary_key= True, editable= False)
     firstName  = models.CharField(max_length= 50, null = False, blank= False)
@@ -25,17 +48,10 @@ class Student(models.Model):
     phone      = models.IntegerField(null=False, blank=False)
     age        = models.IntegerField(null = False, blank= False)
     sex        = models.CharField(max_length=1, null = False, blank= False)
-    class_room_id = models.ForeignKey(Class_room, on_delete=models.SET_NULL, blank=False, null=True)
+    class_room_id = models.ForeignKey(Class_room, on_delete=models.SET_NULL, null=True)
     family_id = models.ForeignKey(Family, on_delete=models.SET_NULL, null=True)
     def __str__(self):
-        return self.firstName + ' ' ##+ self.family_id.__str__()
-
-class Family_type(models.Model):
-    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    typeName = models.CharField(max_length=50, null=False, blank=False)
-    def __str__(self):
-        return self.typeName
-
+        return self.firstName
 class Subject(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     subjectName = models.CharField(max_length=50, null=False, blank=False)
@@ -44,13 +60,7 @@ class Subject(models.Model):
 
 class Teacher(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
-    firstName = models.CharField(max_length=50, null=False, blank=False)
-    middelName = models.CharField(max_length=50, null=False, blank=False)
-    lastName = models.CharField(max_length=50, null=False, blank=False)
-    email = models.EmailField(max_length=100, null=False, blank=False)
-    phone = models.IntegerField( null=False, blank=False)
-    sex = models.CharField(max_length=1, null=False, blank=False)
-
+    employees_id = models.ForeignKey(Employees, on_delete= models.SET_NULL, null=True)
     def __str__(self):
         return self.firstName
 class Class_subject(models.Model):
